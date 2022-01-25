ecc3e9684c65750d942e3c22dfe23cedcec52f86
CB0078 <colinscruz@gmail.com>
2022-01-24 16:06:09 -0800

    changed to numbers guessing game

--- madlibs.py
+++
@@ -1,5 +0,0 @@
-"""THis is a new file"""
-
-"""Let's see if it made a difference"""
-
-# ok lets update from pycharm
---
+++ number_guess.py
@@ -0,0 +1,18 @@
+import random
+
+# User must give number between two certain values until correct.
+
+def number_guess(x, y):
+    answer = random.randint(x, y)
+    guess = 0.1
+
+    while guess != answer:
+        guess = int(input(f"Enter a number between {x} and {y}: "))
+        if guess > answer:
+            print("Too high, guess again!")
+        elif guess < answer:
+            print("Too low, guess again!")
+        else:
+            print(f"You guessed correctly! The secret number is {answer}.")
+
+number_guess(0, 10)
