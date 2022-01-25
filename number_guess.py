c7c2c0574db1e3081fd38a088573132d8ec73483
CB0078 <colinscruz@gmail.com>
2022-01-24 18:28:18 -0800

    added second function for computer to guess

--- number_guess.py
+++ number_guess.py
@@ -2,7 +2,7 @@ import random
 
 # User must give number between two certain values until correct.
 
-def number_guess(x, y):
+def person_guess(x, y):
     answer = random.randint(x, y)
     guess = 0.1
 
@@ -15,4 +15,34 @@ def number_guess(x, y):
 
     print(f"You guessed correctly! The secret number is {answer}.")
 
-number_guess(0, 10)
+
+def computer_guess(answer):
+    """Computer must guess number b/w 1 and 10."""
+
+    guess_count = 0
+    computer_guess = random.randint(1, 10)
+
+    while guess_count < 3:
+        guess_count += 1
+        if computer_guess < answer:
+            print(f"Guess {guess_count}: {computer_guess} --> too low")
+            computer_guess = random.randint(computer_guess + 1, 10)
+        elif computer_guess > answer:
+            print(f"Guess {guess_count}: {computer_guess} --> too high")
+            computer_guess = random.randint(1, computer_guess - 1)
+        else:
+            print(f"Guess {guess_count}: {computer_guess} --> correct! \n"
+                  f"Was able to guess on attempt {guess_count}.")
+            break
+
+    if computer_guess != answer:
+        print(f"Could not guess.")
+
+computer_guess(5)
+
+
+
+
+
+
+
